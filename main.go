0eb44549f4d4eaa07a20613b71071d2c401790de
repl.it user <replituser@example.com>
2020-10-16 04:52:15 +0000

    entrega de slice

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+  var s []int;
+  var tam, temp, total int;
+
+  fmt.Scan(&tam);
+  
+  for i:= 0; i < tam; i++{
+    fmt.Scan(&temp)
+    s = append(s, temp)
+  }
+  
+  for _, v := range s {
+    total += v
+  }
+  
+  fmt.Println(total)
 }
